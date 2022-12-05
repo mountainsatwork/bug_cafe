6123f966e28668a37e8d2efbcc65b172e296e611
minato <km.minato@icloud.com>
2022-12-05 19:01:51 +0900

    orderの各変数名変更

--- cafe.rb
+++ cafe.rb
@@ -24,11 +24,11 @@ def take_order(menus)
   order_number
 end
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+drinks_order = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+foods_order = take_order(FOODS)
 
 
-total = DRINKS[order1][:price].to_i + FOODS[order2][:price].to_i
+total = DRINKS[drinks_order][:price].to_i + FOODS[foods_order][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
